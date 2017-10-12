11a8b0c3b1889d8e6bc0fc21e7f20a6a1d626e06
Olarn U <olarn.u@gmail.com>
2017-10-12 14:12:13 +0700

    add start lab

---
+++ _RxSwiftPlayground/RxSwift.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Please build the scheme 'RxSwiftPlayground' first
+import PlaygroundSupport
+import RxSwift
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+example(of: "Hello, World") { 
+    print("Hello, RxSwift")
+}
